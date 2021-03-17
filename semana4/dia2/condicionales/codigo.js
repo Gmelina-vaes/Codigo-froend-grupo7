dbc01598aa058f00b313e4170272bc23ec8e1ba7
Melina <ing.melina.v.e@gmail.com>
2021-03-16 19:24:49 -0500

    condicionales

---
+++ semana4/dia2/condicionales/codigo.js
@@ -0,0 +1,65 @@
+// condicionales
+
+/*if (se cumple esta condicion){
+    // micodigo se ejecuta si mi condicion es verdadera
+} else if(si se cumple esta otra condicion) {
+    
+} else {
+    }
+
+*/
+
+/* pseudocodigo (podemos hacer  , es lo q podemoshacer  las personas)
+si(una condicion es cierta){
+    ejecuto un codigo aqui
+}
+sino {
+     ejecuta la condcion falsa
+}
+
+*/
+/* pseudocodigo multiple (podemos hacer  , es lo q podemoshacer  las personas)
+si(una condicion es verdadera){
+    Hago este  este codigo
+}
+sino {
+     ejecuta la condcion falsa
+}
+ al poner ===  evaluo el contenido y el tipo
+*/
+let condicion=false;
+if(condicion ===true){
+   console.log("Hola amigos, Bienvenidos a COdigo !!")
+}else{
+    console.log("Amigo que estas esperando ñ_ñ")
+}
+
+// ejercicio
+let a=0;  // creo la variable
+let b=0;
+a=4; // aqui la asigno
+b=5;
+let suma = a+b;
+if (suma ===7) { // se compara y evalua
+    alert(" correcto ! es la respuesta")
+} else {
+
+    alert("¡no esta en lo correcto!")
+}
+// evalua mayusculas y minusculas
+
+let lenguaje_de_programacion ="C++"
+if(lenguaje_de_programacion =="python") 
+  console.log("pyton es asombroso"); 
+  else if(lenguaje_de_programacion =="javascript"){
+      console.log("javascript tambien lo es")
+  }else if (lenguaje_de_programacion=="php") {
+      console.log("mejor apaga la compu")
+  }else if (lenguaje_de_programacion=="C++") {
+      console.log("tu eres d elos bravos")
+  }else (lenguaje_de_programacion =="")
+  {
+      console.log( "No sabes ninguno")
+  }
+
+
